08d7b7e837d27a2241adf834fe226fce6e2e75a8
bolugbolasire-max <bolugbolasire@gmail.com>
2026-07-10 15:14:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -37,3 +37,10 @@ function typeEffect(){
 }
 
 typeEffect();
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
